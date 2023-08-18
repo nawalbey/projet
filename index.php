88bfb88885befcf5f3c9ad2da7b5779e1fbd2867
nawal <nbey34@gmail.com>
2023-08-18 08:55:07 +0200

    php 2 eme jour

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo '<p> bonjour tout le monde <p>';
+    $prenom = "karima"; // string
+    $nom = "dupont"; // string
+    $number = 100; // int
+    $num2 = 1.4; // float
+    $bool = true; //boolean
+    $tab = [
+        'nom' => 'test',
+        'prenom' => 'test',
+
+    ]; //array
+    
+    $tab = ['test', 'test2', 'test3'];
+
+
+    ?>
+
+
+</body>
+
+</html>
